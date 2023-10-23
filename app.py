de4b240ead645af07a7bc35ef699a3bf0019fd6e
Ahmed-Mohamed0Ibrahim <70601256+Ahmed-Mohamed0Ibrahim@users.noreply.github.com>
2023-10-23 16:51:33 +0300

    prepare the web application second part

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import json
+
 import pickle
 from flask import Flask,request,app,jsonify,url_for,render_template
 import pandas as pd
@@ -10,6 +10,9 @@ regmodel = pickle.load(open("regmodel.pkl","rb"))
 #load preprocessing
 preprocessing_pipe = pickle.load(open("preprocessing.pkl","rb"))
 
+#columns names
+columns=["Year","Present_Price","Driven_kms","Fuel_Type","Selling_type","Transmission","Owner"]
+
 @app.route('/')
 def home():
     return render_template("home.html")
@@ -18,16 +21,22 @@ def home():
 def predict_api():
     data=request.json['data']
     print(data)
-    x_x = np.array([list(data.values())])
-    columns=["Year","Present_Price","Driven_kms","Fuel_Type","Selling_type","Transmission","Owner"]
-    new_val = pd.DataFrame(x_x,columns=columns)
+    data = np.array([list(data.values())])
+    new_val = pd.DataFrame(data,columns=columns)
     output=regmodel.predict(preprocessing_pipe.transform(new_val))
     print(output[0])
     return jsonify(output[0])
 
 @app.route("/predict",methods=['POST'])
 def predict():
-    data = [request.form.values()]
+    # get the values from the form
+    data = np.array([list(request.form.values())],dtype="object")
+    # convert the numirical values to float
+    data[:,0:3] = data[:,0:3].astype(np.float_)
+    new_val = pd.DataFrame(data,columns=columns)
+    output=regmodel.predict(preprocessing_pipe.transform(new_val))
+    print("The car price prediction is :",output[0])
+    return render_template("home.html",Prediction_text="The car price prediction is : {}".format(output[0]))
 
 if __name__=="__main__":
     app.run(debug=True)
